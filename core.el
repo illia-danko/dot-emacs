0aa7048484acb99fd723a7095b9b9865958be709
Elijah Danko <me@elijahdanko.net>
2022-06-10 01:10:18 +0200

    Capitalize commit message on push-notes

--- core.el
+++ core.el
@@ -147,7 +147,7 @@
   :after (project)
   :hook (git-commit-setup . flyspell-mode)
   :config
-  (defun magit:auto-notes ()
+  (defun magit:push-notes ()
     "Stage, commit and push upstream a personal note file."
     (interactive)
     (let ((org-dir (expand-file-name org-directory))
@@ -155,13 +155,14 @@
           (relname (file-name-nondirectory (buffer-file-name))))
       (when (string-match-p (regexp-quote org-dir) fullname)
         (magit-stage-file fullname)
-        (magit-commit-create (list "-m" (format "update %s" relname)))
+        (magit-commit-create (list "-m" (format "Update %s" relname)))
         (call-interactively 'magit-push-current-to-upstream))))
+
   :bind (("C-c g g" . magit-status)
          ("C-c g b" . magit-blame-addition)
          ("C-c g d" . magit-diff-buffer-file)
          ("C-c g l" . magit-log)
-         ("C-c g n" . magit:auto-notes)))
+         ("C-c g n" . magit:push-notes)))
 
 ;; (emacs27.2): fix Symbol's value as variable is void: project-switch-commands
 ;; error.
