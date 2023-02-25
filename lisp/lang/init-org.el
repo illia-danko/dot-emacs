aebe436c7f2c0bbd93c0c81353c21dffe881e48a
illiadanko <illia.a.danko@gmail.com>
2023-02-25 02:32:34 +0100

    Fix org-capture diary

--- lisp/lang/init-org.el
+++ lisp/lang/init-org.el
@@ -1,18 +1,22 @@
 (use-package org :straight t
+  :init
+  (defun my-org-capture-todo () (interactive) (org-capture nil "t"))
+  (defun my-org-capture-diary () (interactive) (org-capture nil "d"))
+
   :bind
-  (("C-c ol" . #'org-todo-list)
-   ("C-c oa" . #'org-agenda)
-   ("C-c ot" . (lambda nil (interactive) (org-capture nil "n")))
-   ("C-c od" . (lambda nil (interactive (org-capture nil "d"))))
-   ("C-c oc" . #'org-capture))
+  (("C-c ol" . org-todo-list)
+   ("C-c oa" . org-agenda)
+   ("C-c ot" . my-org-capture-todo)
+   ("C-c od" . my-org-capture-diary)
+   ("C-c oc" . org-capture))
 
   :custom
   (org-directory "~/github.com/illia-danko/org")
   (org-agenda-files (list org-default-notes-file (expand-file-name "diary.org" org-directory)))
   (org-capture-bookmark nil) ; do not keep bookmarks
   (org-capture-templates
-   `(("n" "[n]ew TODO item" entry (file org-default-notes-file) "* TODO %?\nEntered on %U")
-     ("d" "[d]iary entry" entry (file (expand-file-name "diary.org" org-directory)) "* %U %?")))
+   `(("t" "[t]odo item" entry (file org-default-notes-file) "* TODO %?\nEntered on %U")
+     ("d" "[d]iary entry" entry (file ,(expand-file-name "diary.org" org-directory)) "* %U %?")))
   (org-default-notes-file (expand-file-name "todos.org" org-directory)))
 
 (provide 'init-org)
