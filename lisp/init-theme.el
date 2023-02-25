d0fc72c8dbd78b1c01f4a0303e7201bbe9288297
illiadanko <illia.a.danko@gmail.com>
2023-02-25 01:38:32 +0100

    Autoload theme based on the system theme variant on MACOS

--- lisp/init-theme.el
+++ lisp/init-theme.el
@@ -7,9 +7,22 @@
 					   "-*-Iosevka Nerd Font Mono-semibold-normal-normal-*-32-*-*-*-m-0-iso10646-1")))
 	  (set-frame-font font-code nil t))
     (toggle-frame-maximized))
+
   :hook
-  (window-setup . my-adjust-created-frame)
-  :config
-  (load-theme 'modus-operandi t nil))
+  (window-setup . my-adjust-created-frame))
+
+;; Load theme based on the system theme's variant on MACOS.
+(use-package frame
+  :if (eq system-type 'darwin)
+  :init
+  (defun my-apply-theme (appearance)
+	"Load theme, taking current system APPEARANCE into consideration.
+See https://github.com/d12frosted/homebrew-emacs-plus for details."
+	(mapc #'disable-theme custom-enabled-themes)
+	(pcase appearance
+      ('light (load-theme 'modus-operandi t))
+      ('dark (load-theme 'modus-vivendi t))))
+
+  (add-hook 'ns-system-appearance-change-functions #'my-apply-theme))
 
 (provide 'init-theme)
