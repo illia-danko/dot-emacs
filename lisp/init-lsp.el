044d7e920fa47efda786d86c35696750699c1d76
illiadanko <illia.a.danko@gmail.com>
2023-02-27 02:06:39 +0100

    LSP (eglot): do not run flymake

--- lisp/init-lsp.el
+++ lisp/init-lsp.el
@@ -1,6 +1,8 @@
 ;; LSP client.
 (use-package eglot :straight t
   :hook
-  ((go-mode) . eglot-ensure))
+  ((go-mode) . eglot-ensure)
+  :custom
+  (eglot-stay-out-of '(flymake)))
 
 (provide 'init-lsp)
