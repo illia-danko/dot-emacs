3b02a9e3005456fbbe7a188924f30ec3d7b1e64f
illiadanko <illia.a.danko@gmail.com>
2023-02-28 23:47:55 +0100

    Update font

--- site-lisp/init-appearance.el
+++ site-lisp/init-appearance.el
@@ -1,19 +1,11 @@
 ;; Strart Emacs maximized, use custom font.
-(use-package frame
-  :init
-  (defun my-adjust-created-frame ()
-	(let ((font-size (pcase system-type
-					   ('darwin 160)
-					   (_ 125)
-					   )))
-	  (custom-set-faces
-	   `(default ((t (:family "Iosevka Nerd Font Mono" :height ,font-size :weight semibold))))
-	   `(fixed-pitch ((t (:family "Iosevka Nerd Font Mono" :height ,font-size :weight semibold))))
-	   `(variable-pitch ((t (:family "Iosevka Nerd Font Mono" :height ,font-size :weight semibold))))))
-    (toggle-frame-maximized))
 
-  :hook
-  (window-setup . my-adjust-created-frame))
+(when window-system
+  (setq my-default-font "-apple-Monaco-medium-normal-normal-*-15-*-*-*-m-0-iso10646-1")
+  (set-face-attribute 'default nil :font my-default-font))
+
+(toggle-frame-maximized)
+
 
 ;; Load theme based on the system theme's variant.
 (use-package auto-dark :straight t
