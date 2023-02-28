8c76ce7791893afa76b94724d15ad507995ba4c9
illiadanko <illia.a.danko@gmail.com>
2023-02-28 12:47:01 +0100

    Add custom `project-switch-project`

--- lisp/init-personal.el
+++ lisp/init-personal.el
@@ -11,4 +11,14 @@
   ("C-w" . my-backward-kill-word-or-region)
   ([remap kill-buffer] . kill-this-buffer))
 
+(use-package project
+  :config
+  (defun project-switch-project (dir)
+	"Override default `project-switch-project' command.
+Instead of prompt the list with commands, directly execute `project-find-file'.
+Behave as `projectile-switch-project'."
+  (interactive (list (project-prompt-project-dir)))
+  (let ((project-current-directory-override dir))
+    (call-interactively 'project-find-file))))
+
 (provide 'init-personal)
