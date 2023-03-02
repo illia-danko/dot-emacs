f69a5c3cffc8d8b9158dae81ec8266db6e4c5581
illiadanko <illia.a.danko@gmail.com>
2023-03-02 21:43:02 +0100

    Use spacemacs themes by default

--- site-lisp/init-appearance.el
+++ site-lisp/init-appearance.el
@@ -1,17 +1,24 @@
 ;; Strart Emacs maximized, use custom font.
 
 (when window-system
-  (setq my-default-font "-apple-Monaco-medium-normal-normal-*-15-*-*-*-m-0-iso10646-1")
-  (set-face-attribute 'default nil :font my-default-font))
+  (setq my-default-font "-*-JetBrainsMono Nerd Font-semibold-normal-normal-*-15-*-*-*-p-0-iso10646-1")
+  (set-face-attribute 'default nil :font my-default-font)
+  (toggle-frame-maximized))
 
-(toggle-frame-maximized)
+(use-package spacemacs-theme :straight t
+  :defer t
+  :config
+  (mapc #'disable-theme custom-enabled-themes))
 
+(load-theme 'spacemacs-dark t)
 
 ;; Load theme based on the system theme's variant.
 (use-package auto-dark :straight t
+  :after (spacemacs-theme)
+  :if (display-graphic-p)
   :custom
-  (auto-dark-light-theme 'modus-operandi)
-  (auto-dark-dark-theme 'modus-vivendi)
+  (auto-dark-light-theme 'spacemacs-light)
+  (auto-dark-dark-theme 'spacemacs-dark)
   :config
   (auto-dark-mode t))
 
