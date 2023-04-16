2cd0006a769c65ad2832be03ed79fc286ed4033f
illiadanko <illia.a.danko@gmail.com>
2023-04-16 15:51:11 +0200

    Add markdown and org fontification

--- site-lisp/lang/init-markdown.el
+++ site-lisp/lang/init-markdown.el
@@ -1,3 +1,16 @@
-(use-package markdown-mode :straight t)
+(use-package markdown-mode :straight t
+  :init
+  (defun my-markdown-toggle-fontifications (&optional arg)
+    "Toggle fontifications on/off."
+    (interactive (list (or current-prefix-arg 'toggle)))
+    (markdown-toggle-markup-hiding arg))
+
+  :bind
+  ("C-c 4" . my-markdown-toggle-fontifications)
+
+  :custom
+  (markdown-fontify-code-blocks-natively t) ; highlight code block syntax
+  (markdown-hide-markup t)                  ; hide urls
+  )
 
 (provide 'init-markdown)
--- site-lisp/lang/init-org.el
+++ site-lisp/lang/init-org.el
@@ -3,10 +3,28 @@
   (defun my-org-capture-todo () (interactive) (org-capture nil "t"))
   (defun my-org-capture-diary () (interactive) (org-capture nil "d"))
 
+  (defun my-org-toggle-fontifications ()
+    "Toggle fontifications on/off.
+The solution taken from
+https://github.com/zaeph/.emacs.d/blob/4548c34d1965f4732d5df1f56134dc36b58f6577/init.el#L3037-L3069"
+    (interactive)
+    ;; Toggle markers.
+    (setq-local org-hide-emphasis-markers
+                (not org-hide-emphasis-markers))
+    ;; Toggle links.
+    (if org-link-descriptive
+        (remove-from-invisibility-spec '(org-link))
+      (add-to-invisibility-spec '(org-link)))
+    (setq-local org-link-descriptive
+                (not org-link-descriptive))
+    ;; Apply changes.
+    (font-lock-fontify-buffer))
+
   :bind
   (("C-c o" . org-agenda)
    ("C-c u" . my-org-capture-todo)
-   ("C-c y" . my-org-capture-diary))
+   ("C-c y" . my-org-capture-diary)
+   ("C-c 4" . my-org-toggle-fontifications))
 
   :custom
   (org-agenda-files (list org-default-notes-file (expand-file-name "diary.org" org-directory)))
@@ -16,6 +34,10 @@
      ("d" "[d]iary entry" entry (file ,(expand-file-name "diary.org" org-directory)) "* %U %?")))
   (org-default-notes-file (expand-file-name "todos.org" org-directory))
   (org-reverse-note-order t)
+  (org-fontify-done-headline t)
+  (org-fontify-quote-and-verse-blocks t)
+  (org-hide-emphasis-markers t)    ; close links, etc.
+  (org-pretty-entities t)          ; show LaTeX-like symbols as UTF-8 characters
 
   :config
   ;; Preload babel. Make possible to evaluate src code block.
