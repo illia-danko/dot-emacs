394eb110a448f1c5a2cc930b367c0f512ce69108
illiadanko <illia.a.danko@gmail.com>
2023-06-09 17:04:37 +0200

    Add elixir repl shortcuts

--- site-lisp/lang/init-elixir.el
+++ site-lisp/lang/init-elixir.el
@@ -2,6 +2,20 @@
   :hook
   (elixir-mode . inf-elixir-minor-mode))
 
-(use-package inf-elixir :straight t)
+(use-package inf-elixir :straight t
+  :init
+  (defun my-inf-elixir-eval (&optional arg)
+    (interactive "p")
+    (if mark-active
+		(inf-elixir-send-region)
+      (inf-elixir-send-buffer)))
+
+  :custom
+  (inf-elixir-on-send-switch-to-repl nil) ; do not auto switch to the repl buffer
+
+  :bind
+  (:map elixir-mode-map
+        ("C-c C-b" . my-inf-elixir-eval)
+        ("C-c C-c" . inf-elixir-send-line)))
 
 (provide 'init-elixir)
