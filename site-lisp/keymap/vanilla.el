0088f9f6b66dc848c48c358a2c3a0f4468730c5b
illiadanko <illia.a.danko@gmail.com>
2023-12-13 00:14:44 +0200

    Fix eval feature keymap issue

--- site-lisp/keymap/vanilla.el
+++ site-lisp/keymap/vanilla.el
@@ -24,38 +24,38 @@
   (global-set-key (kbd "C-c SPC")                           #'consult-mark)
   (global-set-key (kbd "C-c v")                             #'consult-imenu)
   (global-set-key (kbd "C-c f")                             #'completion/consult-ripgrep)
-  (global-set-key (kbd "C-c F")                             #'completion/consult-line-multi)
+  (global-set-key (kbd "C-c F")                             #'completion/consult-line-multi))
 
-  (with-eval-after-load 'tool/core
-	(global-set-key (kbd "C-c b") #'recompile)
-	(global-set-key (kbd "C-c B") #'project-compile))
+(with-eval-after-load 'tool/core
+  (global-set-key (kbd "C-c b") #'recompile)
+  (global-set-key (kbd "C-c B") #'project-compile))
 
-  (with-eval-after-load 'tool/filesystem
-	(global-set-key [remap dired] #'dired-jump))
+(with-eval-after-load 'tool/filesystem
+  (global-set-key [remap dired] #'dired-jump))
 
-  (with-eval-after-load 'edit/core
-	;; As the tmux config uses C-SPS as a prefix key we need to rebind `set-mark-command'.
-	(define-key core/intercept-mode-map (kbd "C-q") #'set-mark-command)
-	(define-key core/intercept-mode-map (kbd "C-w") #'edit/backward-kill-word-or-region))
+(with-eval-after-load 'edit/core
+  ;; As the tmux config uses C-SPS as a prefix key we need to rebind `set-mark-command'.
+  (define-key core/intercept-mode-map (kbd "C-q") #'set-mark-command)
+  (define-key core/intercept-mode-map (kbd "C-w") #'edit/backward-kill-word-or-region))
 
-  (with-eval-after-load 'edit/vanilla
-	(define-key core/intercept-mode-map (kbd "C-o") #'er/expand-region)
-	(define-key core/intercept-mode-map (kbd "M-o") #'er/contract-region)
-	(global-set-key (kbd "C-c m") #'edit/multiple-cursors-keymap/body))
+(with-eval-after-load 'edit/vanilla
+  (define-key core/intercept-mode-map (kbd "C-o") #'er/expand-region)
+  (define-key core/intercept-mode-map (kbd "M-o") #'er/contract-region)
+  (global-set-key (kbd "C-c m") #'edit/multiple-cursors-keymap/body))
 
-  (with-eval-after-load 'tool/version-control
-	(global-set-key (kbd "C-x g" ) #'magit-status)
-	(global-set-key (kbd "C-c gg") #'magit-status)
-	(global-set-key (kbd "C-c gd") #'magit-diff-buffer-file)
-	(global-set-key (kbd "C-c gb") #'magit-log-buffer-file)
-	(global-set-key (kbd "C-c gL") #'magit-log-all)
-	(global-set-key (kbd "C-c ga") #'magit-blame-addition)
-	(global-set-key (kbd "C-c gr") #'magit-diff-range) ; show difference between branches
-	(global-set-key (kbd "C-c gf") #'magit-find-file) ; visit a file from any branch
-	(global-set-key (kbd "C-c gu") #'git-link)
-	(global-set-key (kbd "C-c gU") #'tool/browse-project-home-page))
+(with-eval-after-load 'tool/version-control
+  (global-set-key (kbd "C-x g" ) #'magit-status)
+  (global-set-key (kbd "C-c gg") #'magit-status)
+  (global-set-key (kbd "C-c gd") #'magit-diff-buffer-file)
+  (global-set-key (kbd "C-c gb") #'magit-log-buffer-file)
+  (global-set-key (kbd "C-c gL") #'magit-log-all)
+  (global-set-key (kbd "C-c ga") #'magit-blame-addition)
+  (global-set-key (kbd "C-c gr") #'magit-diff-range) ; show difference between branches
+  (global-set-key (kbd "C-c gf") #'magit-find-file) ; visit a file from any branch
+  (global-set-key (kbd "C-c gu") #'git-link)
+  (global-set-key (kbd "C-c gU") #'tool/browse-project-home-page))
 
-  (with-eval-after-load 'text/org
-	(global-set-key (kbd "C-c gc") #'org/git-push-org-file))
+(with-eval-after-load 'text/org
+  (global-set-key (kbd "C-c gc") #'org/git-push-org-file))
 
-  (provide 'keymap/vanilla)
+(provide 'keymap/vanilla)
