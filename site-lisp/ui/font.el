9c3c2e4bbdd942ae1ea3cb3fe7e75cf9150e80c5
illiadanko <illia.a.danko@gmail.com>
2023-12-13 13:05:42 +0200

    Change to global system monospace font

--- site-lisp/ui/font.el
+++ site-lisp/ui/font.el
@@ -2,7 +2,7 @@
   (mapc (lambda (face)
           (set-face-attribute face nil
 							  :weight 'bold
-							  :family (or (and (eq system-type 'darwin) "JetBrainsMono Nerd Font") "JetBrainsMonoNL NF")
+							  :family (or (and (eq system-type 'darwin) "JetBrainsMono Nerd Font") "JetBrainsMono NFM")
 							  :height (or (and (eq system-type 'darwin) 135) 110)))
         [default variable-pitch fixed-pitch fixed-pitch-serif]))
 
