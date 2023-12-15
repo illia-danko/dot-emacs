f5908a0dad1a2decfd7910d06daec15b7d2dff3b
illiadanko <illia.a.danko@gmail.com>
2023-12-15 23:05:44 +0200

    Cosmetic: format

--- site-lisp/lang/emacs-lisp.el
+++ site-lisp/lang/emacs-lisp.el
@@ -5,8 +5,8 @@
 
 (with-eval-after-load 'edit/formatting
   (setq format-all-default-formatters
-	(api/upsert-car-string format-all-default-formatters
-				 '("Emacs Lisp" emacs-lisp)))
+		(api/upsert-car-string format-all-default-formatters
+							   '("Emacs Lisp" emacs-lisp)))
 
   (add-hook 'emacs-lisp-mode-hook #'format-all-mode))
 
