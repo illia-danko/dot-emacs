aaaa39eeb05b4b858a1f0c8ec65835011b873622
illiadanko <illia.a.danko@gmail.com>
2023-12-16 18:58:32 +0200

    Remove redundancy

--- site-lisp/lang/emacs-lisp.el
+++ site-lisp/lang/emacs-lisp.el
@@ -3,9 +3,6 @@
 (require 'edit/formatting)
 
 (with-eval-after-load 'elisp-mode
-  (setq format-all-default-formatters
-		(api/upsert-car-string format-all-default-formatters
-							   '("Emacs Lisp" emacs-lisp)))
   (add-hook 'emacs-lisp-mode-hook #'format-all-mode)
   (add-hook 'emacs-lisp-mode-hook #'rainbow-delimiters-mode))
 
