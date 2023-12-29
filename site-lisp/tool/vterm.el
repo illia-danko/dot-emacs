f1063aa34b0846e215ef9b42442d135ab8b39737
illiadanko <illia.a.danko@gmail.com>
2023-12-29 13:52:58 +0200

    Increase number of lines for vterm terminal buffer

--- site-lisp/tool/vterm.el
+++ site-lisp/tool/vterm.el
@@ -1,9 +1,12 @@
 (require 'vterm)
 (require 'core/project)
+(require 'api/macro)
 
 (defun tool/vterm-project (&optional args)
   (interactive)
   (let ((default-directory (core/project-root)))
 	(vterm)))
+
+(api/customize-set-variable* 'vterm-max-scrollback 10000) ; terminal buffer lines number
 
 (provide 'tool/vterm)
