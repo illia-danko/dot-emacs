97a8691e26bf58fcef5fac7e144bd90e82e56e9a
illiadanko <illia.a.danko@gmail.com>
2023-12-30 13:20:25 +0200

    LSP eglot: disable eldoc

--- site-lisp/completion/lsp.el
+++ site-lisp/completion/lsp.el
@@ -2,7 +2,7 @@
 (require 'flymake)
 (require 'api/macro)
 
-(api/customize-set-variable* 'eglot-stay-out-of '(flymake))
+(api/customize-set-variable* 'eglot-stay-out-of '(flymake eldoc))
 
 ;; Override `eglot-inlay-hints-mode' to avoid warning of the not used feature.
 (defun eglot-inlay-hints-mode (&optional mode))
