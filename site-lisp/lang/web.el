238a4b3d7f3c7efce51228ae521e8b5978af6135
illiadanko <illia.a.danko@gmail.com>
2024-01-15 18:14:07 +0100

    Fix javascript file detection

--- site-lisp/lang/web.el
+++ site-lisp/lang/web.el
@@ -23,11 +23,8 @@
 (add-hook 'typescript-ts-mode-hook #'format-all-mode)
 
 ;; Javascript.
-(add-to-list 'treesit-load-name-override-list '(js "libtree-sitter-js" "tree_sitter_javascript"))
-(add-to-list 'treesit-language-source-alist '(js "https://github.com/tree-sitter/tree-sitter-javascript" "master" "src"))
-(add-to-list 'auto-mode-alist '("\\.js\\'" . js-ts-mode))
 (api/customize-set-variable* 'js-indent-level 2)
-(add-hook 'js-ts-mode-hook #'eglot-ensure)
-(add-hook 'js-ts-mode-hook #'format-all-mode)
+(add-hook 'js-mode-hook #'eglot-ensure)
+(add-hook 'js-mode-hook #'format-all-mode)
 
 (provide 'lang/web)
