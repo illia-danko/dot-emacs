fd0cc12e51b836df53f1cfef97ca6f9a45d333a1
illiadanko <illia.a.danko@gmail.com>
2024-01-15 19:15:53 +0100

    Org: auto indent levels

--- site-lisp/text/org.el
+++ site-lisp/text/org.el
@@ -49,7 +49,9 @@ https://github.com/zaeph/.emacs.d/blob/4548c34d1965f4732d5df1f56134dc36b58f6577/
  'org-fontify-quote-and-verse-blocks t
  'org-hide-emphasis-markers t    ; close links, etc.
  'org-pretty-entities t          ; show LaTeX-like symbols as UTF-8 characters
- 'org-agenda-skip-function-global '(org-agenda-skip-entry-if 'todo 'done)) ; filter out org-agenda DONE entries
+ 'org-agenda-skip-function-global '(org-agenda-skip-entry-if 'todo 'done)  ; filter out org-agenda DONE entries
+ 'org-startup-indented t
+ )
 
 (add-hook 'org-mode-hook #'outline-hide-body) ; fold the document on load
 
