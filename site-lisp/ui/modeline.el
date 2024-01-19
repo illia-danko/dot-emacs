821cc74e37fe0f838382679a9a253f9a6f01ee6d
illiadanko <illia.a.danko@gmail.com>
2024-01-19 21:30:58 +0100

    Enable icons on modeline

--- site-lisp/ui/modeline.el
+++ site-lisp/ui/modeline.el
@@ -2,8 +2,7 @@
 (require 'api/macro)
 
 (api/customize-set-variable* 'doom-modeline-height 28
-							 'doom-modeline-bar-width 4
-							 'doom-modeline-icon nil)
+							 'doom-modeline-bar-width 4)
 (doom-modeline-mode 1)
 
 (provide 'ui/modeline)
