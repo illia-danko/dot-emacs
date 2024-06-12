8bd9a0f22ca7ce5e930958d69904199eea33d97c
illiadanko <illia.a.danko@gmail.com>
2024-06-12 18:29:31 +0300

    Add Nixos development tools

--- site-lisp/lang/nix.el
+++ site-lisp/lang/nix.el
@@ -1,5 +1,14 @@
 (require 'nix-mode)
+(require 'edit/formatting)
+(require 'api/macro)
 
 (add-to-list 'auto-mode-alist '("\\.nix\\'" . nix-mode))
+
+(add-hook 'nix-mode-hook #'eglot-ensure)
+(add-hook 'nix-mode-hook #'format-all-mode)
+
+(setq format-all-default-formatters
+	  (api/upsert-car-string format-all-default-formatters
+							 '("Nix" nixfmt)))
 
 (provide 'lang/nix)
