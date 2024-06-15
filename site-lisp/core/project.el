723aa260b19d149aa49cdab77ba8cc921d977a9e
illiadanko <illia.a.danko@gmail.com>
2024-06-15 13:09:12 +0300

    Add Makefile and go.mod to project-root patterns

--- site-lisp/core/project.el
+++ site-lisp/core/project.el
@@ -6,6 +6,32 @@
 (add-to-list 'project-switch-commands '(rg "RipGrep"))
 (add-to-list 'project-switch-commands '(project-compile "Build"))
 
+;;;;;;;;;; Extends core/project-root ;;;;;;;;;;;;;
+;;
+;; A credit to https://andreyor.st/posts/2022-07-16-project-el-enhancements
+
+(defcustom core/project-root-markers
+  '("Makefile" "go.mod")
+  "Files or directories that indicate the root of a project."
+  :type '(repeat string)
+  :group 'project)
+
+(defun core/project-root-p (path)
+  "Check if the current PATH has any of the project root markers."
+  (catch 'found
+    (dolist (marker core/project-root-markers)
+      (when (file-exists-p (concat path marker))
+        (throw 'found marker)))))
+
+(defun core/project-find-root (path)
+  "Search up the PATH for `project-root-markers'."
+  (when-let ((root (locate-dominating-file path #'core/project-root-p)))
+    (cons 'transient (expand-file-name root))))
+
+(add-to-list 'project-find-functions #'core/project-find-root)
+
+;;;;;;;;;;;;;;;;;;;; end ;;;;;;;;;;;;;;;;;;;;;;;;
+
 (defun core/project-root ()
   (or (ignore-errors (project-root (project-current)))
 	  default-directory))
