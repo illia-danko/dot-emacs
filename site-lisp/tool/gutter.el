661db985fbf89e256d404f918cbe694fc408948b
illiadanko <illia.a.danko@gmail.com>
2024-09-12 00:52:43 +0300

    Use git-gutter over git-gutter-fringe

--- site-lisp/tool/gutter.el
+++ site-lisp/tool/gutter.el
@@ -1,39 +1,17 @@
-;; Git gutter fringe UI version.
-(require 'git-gutter-fringe)
+;; Git gutter.
+(require 'git-gutter)
 
 (defun tool/git-gutter-popup-hunk-jump (&optional diffinfo)
   (interactive)
   (git-gutter:popup-hunk diffinfo)
   (switch-to-buffer-other-window git-gutter:popup-buffer))
 
-;; Enable fringe on the right side.
-(fringe-mode '(0 . 8))
-
-(mapc (lambda (fringe-face)
-		(fringe-helper-define fringe-face nil
-		  "....XXXX"
-		  "....XXXX"
-		  "....XXXX"
-		  "....XXXX"
-		  "....XXXX"
-		  "....XXXX"
-		  "....XXXX"
-		  "....XXXX"
-		  "....XXXX"
-		  "....XXXX"
-		  "....XXXX"
-		  "....XXXX"
-		  "....XXXX"
-		  "....XXXX"
-		  "....XXXX"
-		  "....XXXX"
-		  "....XXXX"
-		  "....XXXX"))
-      [git-gutter-fr:added git-gutter-fr:deleted git-gutter-fr:modified])
-
 (api/customize-set-variable*
- 'git-gutter-fr:side 'right-fringe
- 'git-gutter:ask-p nil)
+ 'git-gutter:ask-p nil
+ 'left-margin-width 1; add space for a git-gutter sign
+ 'git-gutter:added-sign ""
+ 'git-gutter:deleted-sign ""
+ 'git-gutter:modified-sign "")
 
 (global-git-gutter-mode 1)
 
