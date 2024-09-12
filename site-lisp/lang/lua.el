39daa05aaac2a9b68bb46949e9f63ecf86c4e266
illiadanko <illia.a.danko@gmail.com>
2024-09-12 11:31:10 +0300

    Update lua completion

--- site-lisp/lang/lua.el
+++ site-lisp/lang/lua.el
@@ -1,3 +1,12 @@
 (require 'lua-mode)
+(require 'completion/lsp)
+(require 'edit/formatting)
+
+(defun lang/lua-setup-hook ()
+  (setq-local tab-width 2)
+  (eglot-ensure)
+  (format-all-mode))
+
+(add-hook 'lua-mode-hook #'lang/lua-setup-hook)
 
 (provide 'lang/lua)
