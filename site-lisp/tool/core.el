9ce650c6e74947012cbcad1ac5ff45c147f503de
illiadanko <illia.a.danko@gmail.com>
2024-09-12 11:31:48 +0300

    Remove unused ttymux-mode

--- site-lisp/tool/core.el
+++ site-lisp/tool/core.el
@@ -1,12 +1,8 @@
 (require 'xclip)
 (require 'compile)
-(require 'ttymux)
 
 ;; xclip.
 (unless (display-graphic-p)
   (xclip-mode 1))
 
-(unless (display-graphic-p)
-  (ttymux-mode 1))
-
-(provide 'tool/core)
+ (provide 'tool/core)
