9e7342e14ed165d4bdfe5fe68157b813b9ba7797
illiadanko <illia.a.danko@gmail.com>
2024-09-26 12:57:41 +0300

    Update orderless theme faces

--- site-lisp/ui/face.el
+++ site-lisp/ui/face.el
@@ -1,6 +1,4 @@
 (require 'doom-themes)
-(require 'orderless)
-(require 'completion)
 
 (defvar ui/theme-dark-variant 'doom-one)
 (defvar ui/theme-light-variant 'doom-one-light)
@@ -18,13 +16,23 @@
 	(ui/customize-theme-frame frame)))
 
 (defun ui/customize-theme-frame (frame)
-  ;; Fix highlighting discrepancy.
+  ;; Adjust border.
   (let ((display-table (or standard-display-table (make-display-table))))
 	(set-display-table-slot display-table 'vertical-border (make-glyph-code ?│)) ; U+2502
 	(setq standard-display-table display-table))
-
   (set-face-attribute 'vertical-border frame
-					  :background (face-background 'default)))
+					  :background (face-background 'default))
+
+  ;; Adjust orderless completion.
+  (mapc (lambda (face-group)
+		  (let ((face (car face-group))
+				(face-ref (cdr face-group)))
+			(set-face-attribute face frame
+								:background (face-background face-ref frame)
+								:foreground (face-foreground face-ref frame)
+								:weight (face-attribute face-ref :weight frame))))
+		[(completions-common-part . orderless-match-face-0)
+		 (completions-first-difference . orderless-match-face-1)]))
 
 (advice-add 'load-theme :around #'(lambda (orig-fun &rest args)
 									(mapc #'disable-theme custom-enabled-themes)
