5974882d2bcf51f42b62b95eaec3fdc17969dcc3
illiadanko <illia.a.danko@gmail.com>
2024-11-26 18:38:40 +0200

    Minor fixes

--- site-lisp/extras/system-theme.el
+++ site-lisp/extras/system-theme.el
@@ -6,12 +6,14 @@
  'auto-dark-dark-theme ui/theme-dark-variant
  'auto-dark-light-theme ui/theme-light-variant)
 
-(add-hook 'auto-dark-dark-mode-hook (lambda ()
-									  (setq catppuccin-flavor 'mocha)
-									  (catppuccin-reload)))
-(add-hook 'auto-dark-light-mode-hook (lambda ()
-									   (setq catppuccin-flavor 'latte)
-									   (catppuccin-reload)))
+(add-hook 'auto-dark-dark-mode-hook
+		  (lambda ()
+			(setq catppuccin-flavor 'mocha)
+			(catppuccin-reload)))
+(add-hook 'auto-dark-light-mode-hook
+		  (lambda ()
+			(setq catppuccin-flavor 'latte)
+			(catppuccin-reload)))
 
 (auto-dark-mode t)
 
